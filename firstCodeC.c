8875fd78090bd69d7db4b19fc357b737852f9ed4
ub-victor <victoireushindi371@gmail.com>
2025-02-20 19:04:31 +0200

    Initial Commit

--- firstCodeC.c
+++ firstCodeC.c
@@ -1,9 +1,14 @@
-// Tis Programme will print the output "Take Courage Victor ,you are making it"
+// This program will print the output "Take Courage Victor, you are making it"
 
+// Include the standard input-output library
 #include <stdio.h>
-int main()
 
+// The main function where the execution of the program begins
+int main()
 {
-    printf ("Take Courage Victor, You are making it")
-    return 0 ;
+    // Print the message to the console
+    printf("Take Courage Victor, You are making it"); // Missing semicolon at the end of this line
+
+    // Return 0 to indicate that the program ended successfully
+    return 0;
 }
